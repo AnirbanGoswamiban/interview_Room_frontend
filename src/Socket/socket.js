e3bcf9d95601505bd4458f9066ee26723525ef74
anirban <anirbangoswami323@gmail.com>
2025-01-01 23:54:42 +0530

    adding hoisted backend server url

--- src/Socket/socket.js
+++ src/Socket/socket.js
@@ -1,4 +1,5 @@
 import { io } from 'socket.io-client';
 // const clientSocket = io('http://localhost:3050'); // Connect to the backend
-const clientSocket = io(`${process.env.REACT_APP_SOCKET_URL}`); // Connect to the backend
+// const clientSocket = io(`${process.env.REACT_APP_SOCKET_URL || 'http://localhost:4000'}`); // Connect to the backend
+const clientSocket = io('https://interview-room-backend.onrender.com')
 export default  clientSocket
